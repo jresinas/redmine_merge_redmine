5d7710d7d053416a27f0567ace7b1cf535d5b675
jresinas <jresinas@emergya.com>
2015-07-29 14:26:27 +0200

    User groups migration

--- app/models/source_user.rb
+++ app/models/source_user.rb
@@ -10,16 +10,33 @@ class SourceUser < ActiveRecord::Base
       if source_user.type == "AnonymousUser"
         user = User.anonymous
         puts "-- Found"
-      elsif user = (User.find_by_mail(source_user.mail) || User.find_by_login(source_user.login)) 
-        puts "-- Found"
-      else
-        user = User.new(source_user.attributes) do |u|
-          u.login = source_user.login
-          u.admin = source_user.admin
-          u.hashed_password = source_user.hashed_password
+      elsif source_user.type == "Group" 
+        user = Group.find_by_lastname(source_user.lastname)
+        
+        if user.present?
+          puts "-- Found Group"
+        else
+          user = Group.new(source_user.attributes) do |g|
+          end
+
+          user.save(false)
+          puts "-- Not found, Group created"
         end
-        user.save(false)
-        puts "-- Not found, created"
+      elsif source_user.type == "User"
+        user = (User.find_by_mail(source_user.mail) || User.find_by_login(source_user.login)) 
+        
+        if user.present?
+          puts "-- Found User"
+        else
+          user = User.new(source_user.attributes) do |u|
+            u.login = source_user.login
+            u.admin = source_user.admin
+            u.hashed_password = source_user.hashed_password
+          end
+
+          user.save(false)
+          puts "-- Not found, User created"
+        end
       end
       puts "-- Added to map"
       RedmineMerge::Mapper.add_user(source_user.id, user.id)
