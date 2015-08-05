ce4f2017351967b65ed951f8d1cc50b888fae770
jresinas <jresinas@emergya.com>
2015-08-05 12:52:54 +0200

    Set groups and users relations

--- app/models/source_user.rb
+++ app/models/source_user.rb
@@ -2,8 +2,9 @@ class SourceUser < ActiveRecord::Base
   include SecondDatabase
   set_table_name :users
 
-  has_many :members
-  has_one :preference, :class_name => 'UserPreference'
+  has_many :members, :class_name => 'SourceMember', :foreign_key => 'user_id'
+  has_one :preference, :class_name => 'SourceUserPreference', :foreign_key => 'user_id'
+  has_and_belongs_to_many :users, :class_name => 'SourceUser', :join_table => 'groups_users', :foreign_key => 'group_id', :association_foreign_key => 'user_id'
 
   def self.migrate
     all.each do |source_user|
@@ -23,6 +24,8 @@ class SourceUser < ActiveRecord::Base
           user.save(false)
           puts "-- Not found, Group created"
         end
+
+        migrate_groups_users(source_user, user.id)
       elsif source_user.type == "User"
         user = (User.find_by_mail(source_user.mail) || User.find_by_login(source_user.login)) 
         
@@ -42,5 +45,28 @@ class SourceUser < ActiveRecord::Base
       puts "-- Added to map"
       RedmineMerge::Mapper.add_user(source_user.id, user.id)
     end
+  end
+
+  def self.migrate_groups_users(source_group, target_group_id)
+    puts "-- Migrating users for group: #{source_group.lastname}"
+    SourceUser.get_group_users(source_group.id).each do |source_user|
+      target_user = User.find(RedmineMerge::Mapper.get_new_user_id(source_user.id))
+      target_group = Group.find(target_group_id)
+      target_group.users << target_user if target_user.present? and target_group.present?
+    end
+  end
+
+  private
+  # Obtiene los usuarios que pertenecen a un grupo en la BBDD de origen
+  def self.get_group_users(group_id)
+    users = []
+    if group_id.present?
+      group = SourceUser.find(group_id)
+      if group.present? and group.type == 'Group'
+        users = SourceUser.find_by_sql "SELECT * FROM users AS u LEFT JOIN groups_users AS gu ON gu.user_id = u.id WHERE gu.group_id="+group_id.to_s
+      end
+    end
+
+    users
   end
 end
