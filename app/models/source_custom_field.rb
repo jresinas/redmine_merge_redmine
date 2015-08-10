2d97508ec105d8cd21518418061f6b2d40587f7c
jresinas <jresinas@emergya.com>
2015-08-10 12:14:43 +0200

    Fix custom field migration

--- app/models/source_custom_field.rb
+++ app/models/source_custom_field.rb
@@ -10,12 +10,12 @@ class SourceCustomField < ActiveRecord::Base
     all.each do |source_custom_field|
       target_custom_field = CustomField.find_by_name(source_custom_field.name)
 
-      if !target_custom_field.present?
+      if !target_custom_field.present? or target_custom_field.type != source_custom_field.type
       	target_custom_field = CustomField.create!(source_custom_field.attributes) do |cf|
           # Type must be set explicitly -- not included in the attributes
-	      cf.type = source_custom_field.type
-	    end
-	  end
+          cf.type = source_custom_field.type
+  	    end
+  	  end
       
       RedmineMerge::Mapper.add_custom_field(source_custom_field.id, target_custom_field.id)
     end
